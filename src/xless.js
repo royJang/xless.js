0c4c3c771c5dca8db971b32b407b3847f58e0b57
roy <zzl11081@gmail.com>
2015-05-17 10:31:35 +0800

    Initial commit

---
+++ src/xless.js
@@ -0,0 +1,22 @@
+/**
+ * Created by roy on 15/5/17.
+ */
+
+var xless = (function (){
+
+    var _x = {};
+
+    _x.show = function (){
+
+    };
+
+    _x.hide = function (){
+
+    };
+
+    return {
+        show : _x.show,
+        hide : _x.hide
+    }
+
+})()
